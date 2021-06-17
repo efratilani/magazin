3c512aeb4e1d02a8b0a5beaf113e9abec3ec0392
chava-p <c0533160506@gmail.com>
2021-06-17 12:30:16 +0300

    add-client

---
+++ my-client/src/components/home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import img from '../assest/image/‏‏p1.JPG'
+import '../style/home.css'
+import {useHistory} from 'react-router-dom'
+
+export default function Home(props) {
+
+
+    const history = useHistory();
+
+    function createPost() {
+        history.push("/create-post");
+    }
+
+    return (
+        <div className="body">
+            <img src={img}></img>
+            <h1>what do you want to do today?</h1>
+            <div className="divContianer">
+                <div className="divButton">
+                    <button onClick={createPost}></button>
+                    <h2>create post</h2>
+                </div>
+                <div className="divButton">
+                    <button></button>
+                    <h2>my megazim</h2>
+                </div>
+            </div>
+        </div>
+    )
+
+}
