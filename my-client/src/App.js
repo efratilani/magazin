9443656d68e221a1ab5cc3413a3a38dfcdd9d421
chava-p <c0533160506@gmail.com>
2021-06-17 13:47:41 +0300

    router

--- my-client/src/App.js
+++ my-client/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
+import {BrowserRouter as Router,
+Switch,Route}  from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
       </div>
   );
 }
