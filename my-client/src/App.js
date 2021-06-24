63f209be3f9b2c865227474a0fbca1f8899c6b3e
YafitPinchasov <yafit.p123000@gmail.com>
2021-06-24 10:48:44 +0300

    ignre

--- my-client/src/App.js
+++ my-client/src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
-import {BrowserRouter as Router,
-Switch,Route}  from 'react-router-dom'
+import {BrowserRouter as Router, Switch,Route}  from 'react-router-dom'
 
 function App() {
   return (
