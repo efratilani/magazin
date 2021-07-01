8a8b33298e53e0f4252dcec0356ffc4c5eaa2e2e
chava-p <c0533160506@gmail.com>
2021-07-01 11:16:08 +0300

    first

--- my-client/src/components/createPost.js
+++ my-client/src/components/createPost.js
@@ -1,30 +1,49 @@
 import React, { useState } from 'react';
-import { Button,Card,Accordion } from "react-bootstrap"
-import {ChevronRight} from 'react-bootstrap-icons'
+import { Button, Card, Accordion } from "react-bootstrap"
+import { ChevronRight ,ChevronDown} from 'react-bootstrap-icons'
 import '../style/style.css'
 
 export default function CreatePost(props) {
-    const [show0,setSh]=useState(false)
-function h()
-{
-    alert()
-}
+    const [show0, setShow0] = useState(false)
+    const [show1, setShow1] = useState(false)
+    const [show2, setShow2] = useState(false)
+    function show(index) {
+        switch (index) {
+            case 0:
+                setShow0(true)
+                setShow1(false)
+                setShow2(false)
+                break;
+            case 1:
+                setShow0(false)
+                setShow1(true)
+                setShow2(false)
+                break;
+            case 2:
+                setShow0(false)
+                setShow1(false)
+                setShow2(true)
+                break;
+
+        }
+
+    }
     return (
         <>
 
             <div className="aside">
                 <Accordion defaultActiveKey="0">
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="0" onClick={h}>
-                        Click me! {<ChevronRight></ChevronRight>}
+                    <Card className="cards">
+                        <Accordion.Toggle as={Card.Header} eventKey="0" onClick={()=>show(0)}>
+                            Click me! {show0?<ChevronDown></ChevronDown>:<ChevronRight></ChevronRight>}
                         </Accordion.Toggle>
                         <Accordion.Collapse eventKey="0">
                             <Card.Body>Hello! I'm the body</Card.Body>
                         </Accordion.Collapse>
                     </Card>
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="1">
-                            Click me! {<ChevronRight></ChevronRight>}
+                    <Card className="cards">
+                        <Accordion.Toggle as={Card.Header} eventKey="1" onClick={()=>show(1)}>
+                            Click me! {show1?<ChevronDown></ChevronDown>:<ChevronRight></ChevronRight>}
                         </Accordion.Toggle>
                         <Accordion.Collapse eventKey="1">
                             <Card.Body>Hello! I'm another body</Card.Body>
